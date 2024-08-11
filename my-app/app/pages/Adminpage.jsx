0aafffa4a4a4635b3ea69112142259c8e9e1daa0
pranavkollara <kollarapranav@gmail.com>
2024-08-11 22:25:51 +0530

    admin page changes

--- my-app/app/pages/Adminpage.jsx
+++ my-app/app/pages/Adminpage.jsx
@@ -1,13 +1,20 @@
 "use client";
 import React, { useEffect, useRef, useState } from "react";
+import { motion, useTime, useTransform } from "framer-motion";
 import Navbar from "../modules/Navbar";
 //serepeb883@alientex.com
+
+
+
 
 export default function Adminpage({ userid, username }) {
   const getdata = useRef(false);
   const paid = useRef(false);
+  
   const [users, setUsers] = useState([]);
   const [sender, setSender] = useState();
+  const [senderbalance, setSenderbalance] = useState();
+  const [receiverbalance, setReceiverbalance] = useState();
   const [receiver, setReceiver] = useState();
   const [amount, setAmount] = useState(0);
   const [info, setInfo] = useState();
@@ -24,13 +31,32 @@ export default function Adminpage({ userid, username }) {
     gett();
   }, []);
 
-  function handlesender(e) {
+  async function handlesender(e) {
     setSender(e.target.value);
     console.log(e.target.value);
+    try{
+
+      const respone = await fetch(`http://localhost:4000/balance/${e.target.value}`);
+      const data = await respone.json();
+      setSenderbalance(data.balance);
+    }catch(err){
+      console.log(err);
+    }
+
+
   }
-  function handlereceiver(e) {
-    setReceiver(e.target.value);
+  async function handlereceiver(e) {
+     setReceiver(e.target.value);
     console.log(e.target.value);
+try{
+
+  const respone = await fetch(`http://localhost:4000/balance/${e.target.value}`);
+  const data = await respone.json();
+  setReceiverbalance(data.balance);
+}catch(err){
+  console.log(err);
+}
+
   }
   function handleamount(e) {
     setAmount(e.target.value);
@@ -50,19 +76,30 @@ export default function Adminpage({ userid, username }) {
       }),
     });
     const data = await respone.json();
-    console.log(data);
+    console.log(data.balance);
     setInfo(data);
+    if(data.Message == "Transaction Completed")
+      {
+    setReceiverbalance(data.Receiver);
+    setSenderbalance(data.Sender); }
   }
+  
 
   return (
     <div className="w-screen h-screen">
       <Navbar userid={userid} username={username}></Navbar>
       <div className="w-screen flex justify-center flex-col items-center gap-4 pt-4">
+        <div>
+        <div className="label">
+    <span className="label-text">Sender</span>
+  </div>
+        
         <select
           onChange={handlesender}
           value={sender}
           className="select select-bordered w-screen max-w-[24rem] "
         >
+          
           <option disabled selected>
             Sender
           </option>
@@ -70,6 +107,14 @@ export default function Adminpage({ userid, username }) {
             return <option value={element.id}>{element.name}</option>;
           })}
         </select>
+        <div className="label">
+    <span className="label-text-alt">Sender Balance : ${senderbalance}</span>
+  </div>
+        </div>
+        <div>
+        <div className="label ">
+    <span className="label-text">Reciever</span>
+  </div>
         <select
           onChange={handlereceiver}
           value={receiver}
@@ -82,6 +127,14 @@ export default function Adminpage({ userid, username }) {
             return <option value={element.id}>{element.name}</option>;
           })}
         </select>
+        <div className="label">
+    <span className="label-text-alt">Reciever Balance : ${receiverbalance}</span>
+  </div>
+        </div>
+        <div>
+        <div className="label">
+    <span className="label-text">Amount</span>
+  </div>
         <input
           type="number"
           placeholder="Type here"
@@ -89,9 +142,10 @@ export default function Adminpage({ userid, username }) {
           className="input input-bordered w-screen max-w-[24rem]"
           onChange={handleamount}
         />
-        <button className="btn btn-primary" onClick={pay}>
-          Pay
-        </button>
+        </div>
+        <motion.button whileHover={{scale:1.1}} className="btn btn-success btn-outline" onClick={pay}>
+          Transact
+        </motion.button>
         <div className=" bg-white w-screen max-w-[24rem] h-fit rounded-lg input-bordered p-4">
           {paid.current && info && (
             <p className="stat-value text-xl text-center">{info.Message}</p>
@@ -107,7 +161,11 @@ export default function Adminpage({ userid, username }) {
             </p>
           )}
         </div>
+      
       </div>
+      
+      
+        
     </div>
   );
 }
